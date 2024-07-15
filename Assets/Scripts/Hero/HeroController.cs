1b61e0b8a65a3b8a17c67e52a585667e57306e9c
mosken <hellsegga@outlook.com>
2024-07-16 03:16:20 +0800

    添加飞船实体与场景碰撞

--- Assets/Scripts/Hero/HeroController.cs
+++ Assets/Scripts/Hero/HeroController.cs
@@ -2,19 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CircleCollider2D))]
+[RequireComponent(typeof(Rigidbody2D))]
 public class HeroController : MonoBehaviour
 {
     public Transform StartPosition;
 
-    public GameObject[] AirshipFrefabs;
+    public GameObject[] AirshipSpirtes;
+
+    private int currAirship;
+    private Rigidbody2D rd;
+
+    private void Awake()
+    {
+        rd = GetComponent<Rigidbody2D>();
+    }
 
     private void Start()
     {
-        // WorldEvents.Instance.OnRestart += Restart();
+        currAirship = 0;
+        WorldEvents.Instance.OnChangeAirship += (id) => currAirship = id;
+        WorldEvents.Instance.OnStart += Show;
+        WorldEvents.Instance.OnDead += Hide;
+        WorldEvents.Instance.OnRestart += Hide;
+        Hide();
     }
 
-    private void Restart()
+    private void Show()
     {
         transform.position = StartPosition.position;
+        gameObject.SetActive(true);
+        for (int i = 0; i < AirshipSpirtes.Length; i++)
+        {
+            AirshipSpirtes[i].SetActive(i == currAirship);
+        }
+    }
+
+    private void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+
     }
 }
